2325c8df5f41b4d92bb222fe5c2b8d85ca638ac2
Aarul Koul <aarulkoul03@gmail.com>
2024-01-06 18:58:35 +0530

    header completed + started intro

---
+++ components/Intro.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            <Image
+              src="/temp-pfp.jpg"
+              alt="Profile Picture"
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="rounded-full h-24 w-24 border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.75,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
