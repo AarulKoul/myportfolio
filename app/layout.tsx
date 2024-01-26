4850bf10f25f5beac92d5892453b72c29286956c
Aarul Koul <aarulkoul03@gmail.com>
2024-01-26 23:08:42 +0530

    Intro Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Aarul | Portfolio",
-  description:
-    "I am an aspiring full stack developer, actively looking for opportunities to work with a team of developers to build amazing products.",
+  description: "My Personal Portfolio Website.",
 };
 
 export default function RootLayout({
@@ -18,10 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] right-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <Header />
         {children}
       </body>
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,16 +3,20 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.2, delay: 0.1 }}
           >
             <Image
               src="/temp-pfp.jpg"
@@ -31,14 +35,63 @@ export default function Intro() {
             transition={{
               type: "spring",
               stiffness: 125,
-              delay: 0.75,
-              duration: 0.7,
+              delay: 0.1,
+              duration: 0.2,
             }}
           >
             👋
           </motion.span>
         </div>
       </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Aarul.</span> I'm an aspiring{" "}
+        <span className="font-bold">full-stack developer</span>, actively
+        looking for opportunities to{" "}
+        <span className="font-bold">build amazing products</span>. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row justify-center items-center gap-3 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        <Link
+          href="#contact"
+          className="group flex gap-2 items-center bg-gray-900 text-white px-7 py-3 outline-none focus:scale-110 hover:scale-110 active:scale-105 rounded-full font-medium text-lg hover:bg-gray-800 transition w-fit"
+        >
+          Contact Me{" "}
+          <BsArrowRight className="opacity-75 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/LatestResume.pdf"
+          download
+          className="group flex bg-white outline-none focus:scale-105 hover:bg-slate-200 hover:scale-110 active:scale-105 transition w-fit text-black px-7 py-3 rounded-full font-medium text-lg items-center gap-2 cursor-pointer border border-black/10"
+        >
+          Download Resume
+          <HiDownload className="opacity-75 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          className="flex bg-white outline-none focus:scale-105 hover:bg-slate-200 hover:scale-110 hover:text-gray-950 active:scale-105 transition text-gray-700 p-4 rounded-full font-medium text-lg items-center gap-2 border border-black/10"
+          href="https://www.linkedin.com/in/aarulkoul/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="flex bg-white outline-none focus:scale-105 hover:bg-slate-200 hover:scale-110 hover:text-gray-950 active:scale-105 transition text-gray-700 p-4 rounded-full font-medium text-xl items-center gap-2 border border-black/10"
+          href="https://github.com/AarulKoul"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
